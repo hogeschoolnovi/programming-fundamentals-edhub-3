02bdd42eaae6028a4ca98ea10a8a8c40bcaf5829
RowanPl <Rowanplooij@hotmail.com>
2024-05-03 11:28:42 +0200

    added solution

--- H6.py
+++ H6.py
@@ -5,13 +5,48 @@
 #     Voeg het getal 6 toe tussen 4 en 7
 #     Vervang het getal 4 door het getal 5
 
+list = [2, 4, 7, 11, 19]
+list.append(22)
+list.insert(2, 6)
+list[1] = 5
+print(list)
+
 # Opgave 2:
 # In de Fibonacci rij bestaat elk getal uit de som van de twee voorgaande getallen: 1, 1, 2, 3, 5…
 # De som van 1 en 1 is 2, de som van 1 en 2 is 3, enzovoorts. Implementeer de functie ‘fibon-acci’ die als
 # parameter de lijst ‘fibonacci_reeks = [1, 1]’ krijgt aangeleverd, en een element toe-voegt aan de lijst,
 # bestaande uit de vorige twee elementen. Roep de functie meerdere keren aan (Bijvoorbeeld met een for-loop).
 
+#Mogelijke oplossing:
+fibbonacci_reeks = [1, 1]
+for i in range(10):
+    a = fibbonacci_reeks[-1]
+    b = fibbonacci_reeks[-2]
+    c = a + b
+    fibbonacci_reeks.append(c)
+print(fibbonacci_reeks)
+
+#Andere oplossing:
+a = 1
+b = 1
+optie2 = [a, b]
+for i in range(10):
+    c = a + b
+    a = b
+    b = c
+    optie2.append(c)
+print(optie2)
+
+# andere oplossing:
+fibbonacci_reeks = [1, 1]
+for i in range(10):
+    fibbonacci_reeks.append(fibbonacci_reeks[-1] + fibbonacci_reeks[-2])
+print(fibbonacci_reeks)
 
 # Opgave 3:
 # Maak een lijst ‘kwadraten’ die de kwadraten bevat van de getallen 1 tot en met 10. Gebruik een for loop.
 
+kwadraten = []
+for i in range(1, 11):
+    kwadraten.append(i ** 2)
+print(kwadraten)
--- H7.py
+++ H7.py
@@ -2,6 +2,10 @@
 # Maak de lijst ‘getal_kwadraat_paar’ aan voor getallen 1 tot en met 5 waarin elk element bestaat uit een
 # tuple die het getal en het bijbehorende kwadraat bevat. Gebruik een list comprehension.
 
+getal_kwadraat_paar = []
+for i in range(1, 6):
+    getal_kwadraat_paar.append((i, i ** 2))
+print(getal_kwadraat_paar)
 
 # Opgave 2:
 # Schrijf een dobbelspel voor een dobbelsteen met waardes 1 t/m 6. Als de speler 5 of 6 gooit,
@@ -9,4 +13,21 @@
 # Gebruik de random.randrange functie uit de module random voor de dobbelsteenwaarde. Hou de game status bij in een
 # variabele die gecheckt wordt door een while loop. Gebruik if/elif waarde in tuple om te checken of een waarde
 # gegooid is. Print de uitkomst.
+
+import random
+
+game_status = True
+while game_status:
+    dobbelsteen = random.randrange(1, 7)
+    if dobbelsteen in (5, 6):
+        print(f"Je hebt gewonnen met {dobbelsteen}")
+        game_status = False
+    elif dobbelsteen in (1, 2):
+        print(f"Je hebt verloren met {dobbelsteen}")
+        game_status = False
+    elif dobbelsteen in (3, 4):
+        print(f"Je mag nog een keer gooien met {dobbelsteen}")
+    else:
+        print(f"Er is iets fout gegaan met {dobbelsteen}")
+        game_status = False
 
