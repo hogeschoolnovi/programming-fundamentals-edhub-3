ec3c7124d6c74c49c49d787998cb2f872b5783e3
Danielooo <dvanraffe@gmail.com>
2024-06-05 16:19:36 +0200

    implemented styling changes in tuples after review

--- tuples.py
+++ tuples.py
@@ -1,10 +1,9 @@
-#####################
-# Voorbeeld opdracht: #
-#####################
-
+# ==========================================
+# Voorbeeld opdracht:
 # Declareer twee tuples met de namen ‘tuple_een’ en ‘tuple_twee’.
 # tuple_een krijgt de waarden 1, 2 en 3. tuple_twee krijgt de waarden 4, 5 en 6.
 # Zorg dat je een tuple krijgt met de waarden 1, 2, 3, 4, 5 en 6. Onthoud dat tuples onveranderlijk zijn.
+# ==========================================
 
 tuple_een = (1, 2, 3)
 tuple_twee = (4, 5, 6)
@@ -15,30 +14,36 @@ combined_tuple = tuple_een + tuple_twee
 print('combined tuple: ', combined_tuple)  # Het resultaat is: (1, 2, 3, 4, 5, 6
 
 
-#####################
 
-# opdracht 1:
-
+# ==========================================
+# Opdracht 1:
 # maak een tuple aan met de waarden 'b', 'c', 'a'
 # print de waarden van de tuple in de juiste volgorde (a, b, c)
 # maak gebruik van de indexen van de tuple
-# gewenste uitkomst:  a b c
+#
+# Gewenste uitkomst:  a b c
+# ==========================================
 
 
-#####################
 
-# opdracht 2:
+# ==========================================
+# Opdracht 2:
 # Maak de lijst ‘getal_kwadraat_paar’ aan voor getallen 1 tot en met 5 waarin elk element bestaat uit een
 # tuple die het getal en het bijbehorende kwadraat bevat. Gebruik een list comprehension.
+#
+# Gewenste uitkomst: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)
+# ==========================================
 
 
 
-#####################
-
-# opdracht 3:
+# =========================================
+# Opdracht 3:
 # Maak een tuple genaamd tuple_count met de waarden 1, 2, 3, 4, 5
 # Zorg dat je een nieuwe tuple_countdown krijgt met de waarden 5, 4, 3, 2, 1
 # Maak gebruik van de reversed() functie
+#
+# Gewenste uitkomst: (5, 4, 3, 2, 1)
+# ==========================================
 
 
 
