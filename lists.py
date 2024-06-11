de8c3167b2657ebc548566bb06bb20416eef6d06
Danielooo <dvanraffe@gmail.com>
2024-06-11 13:26:24 +0200

    resolved PR comments

--- lists.py
+++ lists.py
@@ -1,11 +1,11 @@
 # ==========================================
 # Voorbeeld Opdracht
-# Maak een lijst `getallen` aan: `getallen = [10, 20, 30]`
+# Maak een lijst aan met daarin de volgende getallen: 10, 20 en 30
 #
 # Voer de volgende opdrachten uit:
 # - Voeg het getal 40 toe aan (het einde van) de lijst
 #
-# Aan het einde print je de lijst `getallen`.
+# Aan het einde print je de lijst 'getallen'.
 # ==========================================
 
 
@@ -20,12 +20,14 @@ print('list na toevoeging 40 aan het einde van de lijst: ', getallen)  # Het res
 # ==========================================
 # Opdracht 1:
 # Maak de lijst ‘getallen’ aan: getallen = [2, 4, 7, 11, 19]
+# Maak een lijst aan met daarin de volgende getallen: 2, 4, 7, 11 en 19
 # Voer de volgende opdrachten uit:
 # - Voeg het getal 22 toe aan (het einde van) de lijst
 # - Voeg het getal 6 toe tussen 4 en 7
 # - Vervang het getal 4 door het getal 5
+# - Print de lijst met getallen
 #
-# Gewenste uitkomst: [2, 5, 6, 7, 11, 19, 22]
+# Verwachte uitkomst: [2, 5, 6, 7, 11, 19, 22]
 # ==========================================
 
 
@@ -35,12 +37,12 @@ print('list na toevoeging 40 aan het einde van de lijst: ', getallen)  # Het res
 # In de Fibonacci rij bestaat elk getal uit de som van de twee voorgaande getallen: 1, 1, 2, 3, 5, 8 enzovoorts
 # De som van 1 en 1 is 2, de som van 1 en 2 is 3, enzovoorts. Implementeer de functie ‘fibonacci’ die een lijst als parameter meekrijgt.
 # Voer de volgende opdrachten uit:
-# - Maak een variabele aan genaamd 'fibonacci_start_reeks' en geef  die de eerste twee elementen van de Fibonacci reeks [1, 1].
+# - Maak een variabele aan genaamd 'fibonacci_start_reeks' en geef  die de eerste twee elementen van de Fibonacci reeks.
 # - Maak een functie genaamd fibonacci die de fibonacci_reeks uitbreidt met een nieuw element.
 # - Roep de functie 5 keer aan (Bijvoorbeeld met een for-loop).
 # - Print de waarde van de fibonacci_reeks
 #
-# Gewenste uitkomst:   [1, 1, 2, 3, 5, 8, 13]
+# Verwachte uitkomst:   [1, 1, 2, 3, 5, 8, 13]
 # ==========================================
 
 
@@ -49,7 +51,7 @@ print('list na toevoeging 40 aan het einde van de lijst: ', getallen)  # Het res
 # Opdracht 3:
 # Maak een lijst ‘kwadraten’ die de kwadraten bevat van de getallen 1 tot en met 10. Gebruik een for loop.
 #
-# Gewenste uitkomst:  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# Verwachte uitkomst:  [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 # ==========================================
 
 
--- tuples.py
+++ tuples.py
@@ -17,10 +17,10 @@ print('combined tuple: ', combined_tuple)  # Het resultaat is: (1, 2, 3, 4, 5, 6
 # ==========================================
 # Opdracht 1:
 # maak een tuple aan met de waarden 'b', 'c', 'a'
-# print de waarden van de tuple in de juiste volgorde (a, b, c)
+# print de waarden van de tuple in de alfabetische volgorde (a, b, c)
 # maak gebruik van de indexen van de tuple
 #
-# Gewenste uitkomst:  a b c
+# Verwachte uitkomst:  a b c
 # ==========================================
 
 
@@ -29,7 +29,7 @@ print('combined tuple: ', combined_tuple)  # Het resultaat is: (1, 2, 3, 4, 5, 6
 # Opdracht 2:
 # Maak de lijst ‘getal_kwadraat_paar’ aan voor getallen 1 tot en met 5 waarin elk element bestaat uit een tuple die het getal en het bijbehorende kwadraat bevat. Gebruik een list comprehension.
 #
-# Gewenste uitkomst: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)
+# Verwachte uitkomst: [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
 # ==========================================
 
 
@@ -37,10 +37,10 @@ print('combined tuple: ', combined_tuple)  # Het resultaat is: (1, 2, 3, 4, 5, 6
 # =========================================
 # Opdracht 3:
 # Maak een tuple genaamd tuple_count met de waarden 1, 2, 3, 4, 5
-# Zorg dat je een nieuwe tuple_countdown krijgt met de waarden 5, 4, 3, 2, 1
-# Maak gebruik van de reversed() functie
+# Draai de volgorde van de getallen om. Zorg dat je een nieuwe tuple krijgt met de waarden 5, 4, 3, 2, 1
+# Tip: Maak gebruik van de reversed() functie
 #
-# Gewenste uitkomst: (5, 4, 3, 2, 1)
+# Verwachte uitkomst: (5, 4, 3, 2, 1)
 # ==========================================
 
 
